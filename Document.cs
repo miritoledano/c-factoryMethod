b2995983757930c9b7a20dd24c95fbf0a1c091d1
miri <mt0548553246@gmail.com>
2024-06-24 20:50:26 +0300

    first commit

---
+++ Document.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+      namespace fff
+{
+
+
+    internal abstract class Document
+    {
+        public abstract void createDocument();
+    }
+    class WordDocument : Document
+    {
+        public override void createDocument()
+        {
+            Console.WriteLine("Creating Concrete word");
+        }
+
+    }
+    class pirepoint : Document
+    {
+        public override void createDocument()
+        {
+            Console.WriteLine("Creating Concrete pirepoint");
+        }
+
+    }
+    class DocumentFactory
+    {
+        public static Document CreateDocument()
+        {
+            string word = ConfigurationManager.AppSettings["word"];
+            string pirepoint = ConfigurationManager.AppSettings["pirepoint"];
+            return CreateDocument(word);
+
+        }
+        public static Document CreateDocument(string type)
+        {
+            switch (type)
+            {
+                case "worddocument":
+                    return new WordDocument();
+                case "pirepoint":
+                    return new pirepoint();
+                default:
+                    throw new ArgumentException("Invalid document type");
+            }
+        }
+    }
+
+
+}
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter document type (worddocument or pirepoint):");
+            //string userInput = Console.ReadLine();
+
+
+            //Document doc = DocumentFactory.CreateDocument(userInput);
+
+
+
+            //doc.createDocument();
+            //Console.ReadLine();
+            //במקרה שמקבל
+
+            Document doc = DocumentFactory.CreateDocument();
+            doc.createDocument();
+          
+
+
+
+
+
+        }
+    }
+}
